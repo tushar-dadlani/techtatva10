93766cb8819ce4cf062ca0560f8c9c99864adb4e
Tushar Dadlani <tush726@gmail.com>
2010-07-20 03:52:48 +0530

    added validations to the student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ActiveRecord::Base
+    validates_presence_of  :name,:college_name,:college_id,:email_address,:contact_number 
+    validates_format_of :email_address, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+    validates_format_of :contact_number , :with => /\A[0-9]+\Z/i , :on => :create
+    validates_length_of :contact_number , :is => 10 
 end
